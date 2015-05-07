b622330aeea78810d395b72ef152630cdaee6b2e
dead cat <dcat@users.noreply.github.com>
2015-05-07 13:58:58 +0200

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 #define INNER		3		/*  inner border width */
 #define OUTER		1		/*  outer border width */
 
-/* default colours, don't worry, you can set these with swc later */
+/* colors */
 #define FOCUSCOL	0x18191A
 #define UNFOCUSCOL	0x111213
 #define OUTERCOL	0x000000
